a2b3e1f3a4d6a720438e951f8717076211a0c345
Joao_Marcelo_PC <joao.marcelo0611@outlook.com>
2024-09-07 23:42:01 -0300

    feat: adicionado card dos clientes

--- src/app/component/client-page/client-page.component.ts
+++ src/app/component/client-page/client-page.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { DefaultHomePageComponent } from '../default-home-page/default-home-page.component';
+import { CommonModule, NgFor } from '@angular/common';
+import { IClient } from './client';
+import { ClientService } from './client.service';
 
 @Component({
   selector: 'app-client-page',
   standalone: true,
-  imports: [DefaultHomePageComponent],
+  imports: [DefaultHomePageComponent, NgFor, CommonModule],
   templateUrl: './client-page.component.html',
   styleUrl: './client-page.component.css'
 })
-export class ClientPageComponent {
+
+export class ClientPageComponent implements OnInit{
+
+  public clients : IClient [] = [];
+
+  constructor(
+    private clientService : ClientService
+  ){}
+
+  ngOnInit(): void {
+    this.clients = this.clientService.getAll();
+  }
 
 }
--- src/app/component/client-page/client.service.ts
+++ src/app/component/client-page/client.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { clients, IClient } from './client';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ClientService {
 
+  private client : IClient [] = clients
+
   constructor() { }
+
+  getAll(){
+    return this.client;
+  }
+
+  getOne(clientId : string){
+    return this.client.find( client => client.id == clientId )
+  }
+
 }
